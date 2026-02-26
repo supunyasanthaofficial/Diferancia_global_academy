f46127c32b0990ecabf9a6491cc063ee2d25db29
supunyasantha <supunyasantha.official@gmail.com>
2026-02-26 15:18:49 +0530

    Import the Navbar and Footer.

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,25 @@ import ApplyNow from "./pages/ApplyNow";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
 import Navbar from "./navigation/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <div className="font-sans text-gray-900 bg-white">
-        <Navbar/>
+        <Navbar />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/course" element={<Course />} />
-            <Route path="/apply-now" element={<ApplyNow />} />
+            <Route path="/apply" element={<ApplyNow />} />
             <Route path="gallery" element={<Gallery />} />
             <Route path="contact" element={<Contact />} />
           </Routes>
         </main>
       </div>
+      <Footer />
     </Router>
   );
 }
