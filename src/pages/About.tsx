94b425bbd829727c1d741c38c065f8e138ca76bb
supunyasantha <supunyasantha.official@gmail.com>
2026-02-27 13:51:51 +0530

    Import the WhatWedo , LectureSection and Partners sections.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,8 @@
 import Header from "../components/Header";
 import Header2 from "../assets/images/Header2.jpg";
+import WhatWeDoSection from "../components/WhatWeDoSection";
+import LecturerSection from "../components/LecturesSection";
+import PartnerSection from "../components/PartnerSection";
 
 const About = () => {
   return (
@@ -11,6 +14,9 @@ const About = () => {
         height="400px"
         showButtons={true}
       />
+      <WhatWeDoSection/>
+      <LecturerSection/>
+      <PartnerSection/>
     </main>
   );
 };
