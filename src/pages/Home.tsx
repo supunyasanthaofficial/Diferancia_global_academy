26c98a95e09540be3fb3de4d38ba07aef2968a27
supunyasantha <supunyasantha.official@gmail.com>
2026-02-27 14:09:02 +0530

    import the Partners section.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,8 @@
 import Header from "../components/Header";
 import Header1 from "../assets/images/Header1.jpg";
+import PartnerSection from "../components/PartnerSection";
+
+
 
 const Home = () => {
   return (
@@ -11,6 +14,7 @@ const Home = () => {
         height="400px"
         showButtons={true}
       />
+    <PartnerSection/>
     </main>
   );
 };
