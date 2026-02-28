241d966ae1c6528b03270cb04525df00833a6de9
supunyasantha <supunyasantha.official@gmail.com>
2026-02-28 11:42:47 +0530

    Add program data and Import and call the Slider.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,27 @@
 import Header from "../components/Header";
 import Header1 from "../assets/images/Header1.jpg";
 import PartnerSection from "../components/PartnerSection";
+import AboutSection from "../components/AboutSection";
+import Slider from "../components/Slider";
 
-
+import E1 from "../assets/images/E1.jpg";
+import E2 from "../assets/images/E2.jpg";
 
 const Home = () => {
+  const OurPrograms = [
+    {
+      title: "Certificate in Front Office Management ",
+      description:
+        "Learn professional cleaning,maintenance,and organizational skills for a safe, efficient workspace",
+      image: E1,
+    },
+    {
+      title: "Certificate in Pastry & Bakery",
+      description:
+        "Learn baking techniques with professional cleaning,maintennance,and ,safe,efficient practises.",
+      image: E2,
+    },
+  ];
   return (
     <main>
       <Header
@@ -14,7 +31,9 @@ const Home = () => {
         height="400px"
         showButtons={true}
       />
-    <PartnerSection/>
+      <AboutSection />
+      <Slider title="Our Programs" data={OurPrograms} />
+      <PartnerSection />
     </main>
   );
 };
