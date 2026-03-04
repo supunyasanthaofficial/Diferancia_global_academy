51d1faa14c9f2f8bb667b3a34cddf82fb5b46de4
supunyasantha <supunyasantha.official@gmail.com>
2026-03-04 09:44:26 +0530

    Updated Certificate and Diploma Pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Course from "./pages/Course";
+// import Course from "./pages/Course";
 import ApplyNow from "./pages/ApplyNow";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
 import Navbar from "./navigation/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
+import Certificate from "./pages/Certificate";
+import Diploma from "./pages/Diploma";
 
 function App() {
   return (
@@ -15,14 +17,16 @@ function App() {
       <div className="font-sans text-gray-900 bg-white">
         <Navbar />
         <main>
-          <ScrollToTop/>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/course" element={<Course />} />
+            {/* <Route path="/course" element={<Course />} /> */}
             <Route path="/apply" element={<ApplyNow />} />
-            <Route path="gallery" element={<Gallery />} />
-            <Route path="contact" element={<Contact />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/course/certificate" element={<Certificate />} />
+            <Route path="/course/diploma" element={<Diploma />} />
           </Routes>
         </main>
       </div>
